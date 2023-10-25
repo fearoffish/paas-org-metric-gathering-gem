88a41b61db95eb2b897ac6b58d58df22fb42cd45
Jamie Van Dyke <jamie.vandyke@digital.cabinet-office.gov.uk>
2023-10-25 13:49:27 +0100

    add the option to see all instances in the services tree

--- lib/jcf/cf/services.rb
+++ lib/jcf/cf/services.rb
@@ -5,13 +5,19 @@ require_relative "base"
 module JCF
   module CF
     class Services
-      def self.first(name:)
+      def self.first(name:, instances: false)
         # Find the broker, then find all offerings for that broker, then all plans
         # { broker: { offerings: { plans: [] } }
         # e.g. { "p-mysql (guid)": { "10mb (guid)": [], "100mb (guid)": [] } }
         broker = ServiceBroker.first(name: name)
         offerings = ServiceOffering.all(service_broker_guids: broker.guid).map do |offering|
           plans = ServicePlan.all(service_offering_guids: offering.guid)
+          if instances
+            plans = plans.map do |plan|
+              instances = ServiceInstance.all(service_plan_guids: plan.guid)
+              { plan.to_s => instances }
+            end
+          end
           { offering.to_s => plans }
         end
         { broker.to_s => offerings }
--- lib/jcf/cli/commands/cf/services.rb
+++ lib/jcf/cli/commands/cf/services.rb
@@ -11,9 +11,11 @@ module JCF
         class Services < Command
           argument :broker, required: true, desc: "Broker name"
 
-          def call(broker:, **)
+          option :instances, type: :boolean, desc: "Show instances for each service (this is an API heavy call)"
+
+          def call(broker:, **options)
             # gather all service offerings and plans for a single broker
-            out.puts formatter.format(data: JCF::CF::Services.first(name: broker), tree: true)
+            out.puts formatter.format(data: JCF::CF::Services.first(name: broker, instances: options[:instances]), tree: true)
           end
         end
       end
